533a25cd0080dc4f3269378f82555f643efdeca0
aras arkan <83475962+imawss@users.noreply.github.com>
2026-01-26 03:17:21 +0300

    first commit

---
+++ src/main/java/frc/robot/constants/AutonomousConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public class AutonomousConstants {
+    public static final double PP_DRIVE_kP = 10;
+    public static final double PP_DRIVE_kI = 0;
+    public static final double PP_DRIVE_kD = 0;
+
+    public static final double PP_STEER_kP = 7;
+    public static final double PP_STEER_kI = 0;
+    public static final double PP_STEER_kD = 0;
+}
