c98f2f1a1c06ee6259a19087ea37c340377d4065
aras arkan <83475962+imawss@users.noreply.github.com>
2026-02-01 23:20:48 +0300

    Paths for testing

---
+++ src/main/java/frc/robot/subsystems/hooper/HooperSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.hooper;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import frc.robot.constants.IntakeConstants;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class HooperSubsystem extends SubsystemBase {
+    
+    public HooperSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
